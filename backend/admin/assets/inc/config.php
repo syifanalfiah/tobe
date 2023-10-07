d7115f5248f023aca9d3eaf7e16235bce00256b7
syifanalfiah <112307944+syifanalfiah@users.noreply.github.com>
2023-10-07 22:25:03 +0700

    Delete backend directory

--- backend/admin/assets/inc/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dbuser="root";
-$dbpass="database-password";
-$host="localhost";
-$db="database-name";
-$mysqli=new mysqli($host,$dbuser, $dbpass, $db);
-?>
--- backend/admin/assets/inc/footer1.php
+++
@@ -1,3 +0,0 @@
-<footer class="footer footer-alt">
-    2020 - <?php echo date('Y'); ?> &copy; Hospital Management System.</a>
-</footer>
